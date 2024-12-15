70bc74ee2159f97964e8441ca62e0ca504a5f137
Micah Longmire <mlmicahlongmire@gmail.com>
2024-12-15 09:35:55 -0700

    Update submodules

--- utils/git_update.sh
+++ utils/git_update.sh
@@ -3,3 +3,10 @@ echo "Pulling latest changes and updating submodules..."
 git pull origin main --recurse-submodules
 git submodule init
 git submodule update --remote --merge
+
+# If there are any changes, commit them
+if [ -n "$(git status --porcelain)" ]; then
+  git add .
+  git commit -m "Update submodules"
+  git push origin main
+fi
